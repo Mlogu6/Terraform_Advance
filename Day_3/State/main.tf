7e3c8ad1366c5abd3f284d141be4e5a01f208614
Logan <mlogan6@gmail.com>
2023-01-10 11:37:53 +0530

    Update Commit 4

--- Day_3/State/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "machine-1" {
-  ami           = var.aws_ami_id
-  instance_type = var.instance_type
-  count         = var.aws_instance_count
-  tags = {
-    Name = "Machine-1"
-  }
-}
